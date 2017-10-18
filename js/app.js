9e35bd3512cda114bd1ce9bfe2f6a5ad326834a1
jennifer carmen <jennifercarmens@gmail.com>
2017-10-18 16:56:01 -0500

    Banderas Display

---
+++ js/app.js
@@ -0,0 +1,36 @@
+function mostrarmenu(){
+  document.getElementById('start').style.display = 'block';
+  document.getElementById('part1').style.display = 'none';
+  document.getElementById('part2').style.display = 'none';
+  document.getElementById('part3').style.display = 'none';
+  document.getElementById('part4').style.display = 'none';
+}
+function mostrarparte1(){
+document.getElementById('start').style.display = 'none';
+document.getElementById('part1').style.display = 'block';
+document.getElementById('part2').style.display = 'none';
+document.getElementById('part3').style.display = 'none';
+document.getElementById('part4').style.display = 'none';
+
+}
+function mostrarparte2(){
+document.getElementById('start').style.display = 'none';
+document.getElementById('part2').style.display = 'block';
+document.getElementById('part1').style.display = 'none';
+document.getElementById('part3').style.display = 'none';
+document.getElementById('part4').style.display = 'none';
+}
+function mostrarparte3(){
+document.getElementById('start').style.display = 'none';
+document.getElementById('part3').style.display = 'block';
+document.getElementById('part1').style.display = 'none';
+document.getElementById('part2').style.display = 'none';
+document.getElementById('part4').style.display = 'none';
+}
+function mostrarparte4(){
+  document.getElementById('start').style.display = 'none';
+document.getElementById('part4').style.display = 'block';
+document.getElementById('part1').style.display = 'none';
+document.getElementById('part3').style.display = 'none';
+document.getElementById('part2').style.display = 'none';
+}
